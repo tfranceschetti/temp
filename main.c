6beaf72ced8371b42806ebaf186f0fb2e326c614
Tobia Franceschetti <163556568+tfranceschetti@users.noreply.github.com>
2024-06-06 12:29:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <stdbool.h>
 
+extern bool palindromo(unsigned long long x);
+
+int main(void)
+{
+	palindromo(122);
+	palindromo(10);
+	palindromo(1);
+	palindromo(809908);
+	palindromo(56765);
+	palindromo(11221);
+	return 0;
+}
